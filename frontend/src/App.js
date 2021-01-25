ffd226dc7d37600ce0320da2e808adb45cff78a2
Jesse <jbenlind@gmail.com>
2021-01-25 17:54:35 -0600

    nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,25 @@
+import { Switch, Route } from "react-router-dom";
+import SignupFormPage from './components/SignupFormPage';
+import LoginFormPage from './components/LoginFormPage';
+import Navigation from './components/Navigation';
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <>
+    <Navigation />
+      <Switch>
+
+        <Route path='/login'>
+          <LoginFormPage />
+        </Route>
+
+        <Route path='/signup'>
+          <SignupFormPage />
+        </Route>
+
+      </Switch>
+    </>
+
   );
 }
 
