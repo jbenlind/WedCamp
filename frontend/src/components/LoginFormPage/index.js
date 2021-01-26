a5d933ee356887e9cb24c7dff5294db5c83c18a0
Jesse <jbenlind@gmail.com>
2021-01-26 15:28:11 -0600

    finished login and signup forms

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -25,33 +25,35 @@ function LoginFormPage() {
   }
 
   return (
-    <form className='login-form' onSubmit={handleSubmit}>
-      <h3 className='form-header'>Welcome back!</h3>
-      <p className='form-text'>Let's get you outside</p>
-      <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-      </ul>
-        <div className="input-fields">
-            <input
-              className='email-input'
-              type="text"
-              value={credential}
-              onChange={(e) => setCredential(e.target.value)}
-              required
-              placeholder='Email address...'
-            />
-            <input
-              className='password-input'
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              placeholder='Password...'
-            />
-          <button className='login-button' type="submit">Log In</button>
-          <NavLink className='redirect-signup' to='/signup'>Don't have an account?</NavLink>
-        </div>
-    </form>
+    <div className='login-page'>
+      <form className='login-form' onSubmit={handleSubmit}>
+        <h3 className='form-header'>Welcome back!</h3>
+        <p className='form-text'>Let's get you outside</p>
+        <ul>
+          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+        </ul>
+          <div className="input-fields">
+              <input
+                className='email-input'
+                type="text"
+                value={credential}
+                onChange={(e) => setCredential(e.target.value)}
+                required
+                placeholder='Email address...'
+              />
+              <input
+                className='password-input'
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                placeholder='Password...'
+              />
+            <button className='login-button' type="submit">Log In</button>
+            <NavLink className='redirect-signup' to='/signup'>Don't have an account?</NavLink>
+          </div>
+      </form>
+    </div>
 
   );
 }
--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { Redirect, NavLink } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 import './SignupForm.css';
 
@@ -28,45 +28,51 @@ function SignupFormPage() {
   };
 
   return (
-    <form className='signup-form' onSubmit={handleSubmit}>
-      <h3>Join WedCamp</h3>
-      <h4>Start discovering venues</h4>
-      <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-      </ul>
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          placeholder='Email'
-        />
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-          placeholder='Username'
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          placeholder='Password'
-        />
-
-      <label>
-        Confirm Password
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Sign Up</button>
-    </form>
+    <div className='signup-page'>
+      <form className='signup-form' onSubmit={handleSubmit}>
+        <h3 className='form-header'>Join WedCamp</h3>
+        <p className='form-text'>Start discovering venues</p>
+        <ul>
+          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+        </ul>
+        <div className='signup-form-body'>
+          <input
+            className='signup-email-input'
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            placeholder='Email...'
+          />
+          <input
+            className='signup-username-input'
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            placeholder='Username...'
+          />
+          <input
+            className='signup-password-input'
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            placeholder='Password...'
+          />
+          <input
+            className='confirm-password-input'
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+            placeholder='Confirm password...'
+          />
+          <button className='signup-button' type="submit">Sign Up</button>
+          <NavLink className='redirect-login' to='/login'>Already have an account?</NavLink>
+        </div>
+      </form>
+    </div>
   );
 }
 
