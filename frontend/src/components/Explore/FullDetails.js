1f6768db4a0e088b0af35ad3241b6085c8fc66c3
Jesse <jbenlind@gmail.com>
2021-01-28 09:50:13 -0600

    changes to explore page

--- frontend/src/components/Explore/FullDetails.js
+++ frontend/src/components/Explore/FullDetails.js
@@ -19,28 +19,35 @@ const FullDetails = () => {
   if (!venue) return null;
 
   return (
-    <div className="venue-detail-page">
-      <div className='details-card'>
-        <div className="image-form">
-          <div className="image-form-details">
-            <h1 className="venue-name">{venue.name}</h1>
-            <div className="venue-items">
-              <li className="city-state">{`${venue.city}, ${venue.state}`}</li>
-              <li className="price">{`Average cost: $${venue.averagePrice}`}</li>
-              <li className="guests">{`Max number of guests: ${venue.maxNumberGuests}`}</li>
-              <li className="zipcode">{`Zipcode: ${venue.zipCode}`}</li>
-              <li className="gpsLocation">{`Cooridinates: ${venue.gpsLocation}`}</li>
-            </div>
-          </div>
+    <div className="venue-details-page">
+      <div className='details-grid'>
+        <div className="image-title-container">
+          <h1 className="venue-name">{venue.name}</h1>
           <img src={venue.imgUrl} alt="hidden"></img>
         </div>
-        <div className='description-booking'>
-          <div className="description">
-            <div className="venue-description">{venue.description}</div>
-          </div>
+        <div className='bookingForm-googleMaps'>
           <form className='booking-form'>
             <h3 className='booking-form-title'>Book</h3>
           </form>
+          <div className='google-maps-api'>
+            {/* google maps box */}
+          </div>
+        </div>
+        <div className="venue-items-container">
+          <li className="city-state">{`${venue.city}, ${venue.state}`}</li>
+          <li className="price">{`Average cost: $${venue.averagePrice}`}</li>
+          <li className="guests">{`Max number of guests: ${venue.maxNumberGuests}`}</li>
+          {/* <li className="zipcode">{`Zipcode: ${venue.zipCode}`}</li>
+          <li className="gpsLocation">{`Cooridinates: ${venue.gpsLocation}`}</li> */}
+        </div>
+        <div className="description-container">
+          <div className="venue-description">{venue.description}</div>
+        </div>
+        <div className='amenities-container'>
+          {/* <div></div> for the amenities */}
+        </div>
+        <div className='reviews-container'>
+          {/* <div></div> for the reviews */}
         </div>
       </div>
     </div>
--- frontend/src/components/Explore/index.js
+++ frontend/src/components/Explore/index.js
@@ -15,16 +15,18 @@ const Explorer = () => {
 
     return(
         <div className='explorer-body'>
-            {venues &&
-            venues.map((venue) => (
-                <Link className ='card-link' to={`/explore/${venue.id}`}>
-                    <div className="venue-card" value={venue.id} key={venue.id}
-                        style={ {backgroundImage: `url(${venue.imgUrl})`}}>
-                        <div className='card-title'>{venue.name}</div>
-                        <div className='card-location'>{`${venue.city}, ${venue.state}`}</div>
-                    </div>
-                </Link>
-            ))}
+            <div className='grid-body'>
+                {venues &&
+                venues.map((venue) => (
+                    <Link className ='card-link' to={`/explore/${venue.id}`}>
+                        <div className="venue-card" value={venue.id} key={venue.id}
+                            style={ {backgroundImage: `url(${venue.imgUrl})`}}>
+                            <div className='card-title'>{venue.name}</div>
+                            <div className='card-location'>{`${venue.city}, ${venue.state}`}</div>
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </div>
     )
 }
