a1c3e96a72513cf8fb798e682b58d9afeead6705
Jesse <jbenlind@gmail.com>
2021-01-28 15:57:29 -0600

    more styling

--- frontend/src/components/Explore/FullDetails.js
+++ frontend/src/components/Explore/FullDetails.js
@@ -22,33 +22,53 @@ const FullDetails = () => {
     <div className="venue-details-page">
       <div className='details-grid'>
         <div className="image-title-container">
-          <h1 className="venue-name">{venue.name}</h1>
-          <img src={venue.imgUrl} alt="hidden"></img>
+          <div className='card-image' >
+            <h1 className="venue-name">{venue.name}</h1>
+            <img className="image" src={venue.imgUrl} alt="hidden"></img>
+          </div>
         </div>
         <div className='bookingForm-googleMaps'>
           <form className='booking-form'>
-            <h3 className='booking-form-title'>Book</h3>
+          <div className="price">{`Average price: $${venue.averagePrice}`}</div>
+            <div>
+              <input
+                  className='email-input'
+                  type="date"
+                  // value={credential}
+                  // onChange={(e) => setCredential(e.target.value)}
+                  required
+                />
+            </div>
+            <button className='book-button' type="submit">Book Venue</button>
           </form>
           <div className='google-maps-api'>
             {/* google maps box */}
           </div>
         </div>
         <div className="venue-items-container">
-          <li className="city-state">{`${venue.city}, ${venue.state}`}</li>
-          <li className="price">{`Average cost: $${venue.averagePrice}`}</li>
-          <li className="guests">{`Max number of guests: ${venue.maxNumberGuests}`}</li>
+          <div className="city-state">{`Location: ${venue.city}, ${venue.state}`}</div>
+          <div className="guests">{`Max number of guests: ${venue.maxNumberGuests}`}</div>
           {/* <li className="zipcode">{`Zipcode: ${venue.zipCode}`}</li>
           <li className="gpsLocation">{`Cooridinates: ${venue.gpsLocation}`}</li> */}
         </div>
         <div className="description-container">
+          <h1 className="description-title">Description:</h1>
           <div className="venue-description">{venue.description}</div>
         </div>
         <div className='amenities-container'>
+          <h1 className="description-title">Amenities:</h1>
+          <p>this is the amenities area</p>
           {/* <div></div> for the amenities */}
         </div>
         <div className='reviews-container'>
+          <h1 className="description-title">Reviews:</h1>
           {/* <div></div> for the reviews */}
         </div>
+        <div className='background-cover'></div>
+        <div className='background-cover-2'></div>
+        <div className='background-cover-3'></div>
+        <div className='background-cover-4'></div>
+        <div className='background-cover-5'></div>
       </div>
     </div>
   );
