9b03e66449f93ec75acfa2556aa892e9c86faa90
Jesse <jbenlind@gmail.com>
2021-01-29 06:18:27 -0600

    changes to seed files

--- backend/db/seeders/20210125223425-amenities.js
+++ backend/db/seeders/20210125223425-amenities.js
@@ -7,7 +7,98 @@ module.exports = {
         name: "Indoor seating",
         createdAt: new Date(),
         updatedAt: new Date()
+      },
+      {
+        name: "Wifi",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Parking",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Yard games",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Prep kitchen",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Restrooms",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Handicap accessible",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Free clean up",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Dance area",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Electricity",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Full bar",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Sound system",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Flatware ",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Venue planner",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Dressing area",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Running water",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Stage",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Smoking allowed",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: "Lighting",
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
+
     ], {});
 
   },
