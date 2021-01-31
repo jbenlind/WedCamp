6c9fa3a01fd0e2a4e0ff35647447baf9daaeb899
Jesse <jbenlind@gmail.com>
2021-01-30 20:17:06 -0600

    changes to routes

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -14,4 +14,12 @@ router.get(
   })
 );
 
+router.post('/:userId',
+  asyncHandler(async (req, res, next) => {
+    const bookingData = req.body.booking;
+    const booking = await Booking.create(bookingData);
+      res.json({ booking });
+
+  }));
+
 module.exports = router;
