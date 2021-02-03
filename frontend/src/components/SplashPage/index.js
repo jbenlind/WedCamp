8a9f92ed0dbb2813e78602a7b1f788ddda1fc5af
Jesse <jbenlind@gmail.com>
2021-02-03 01:25:02 -0600

    callendar styles

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -2,6 +2,9 @@ import React, { useState} from "react";
 import {useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import * as venueActions from "../../store/venues";
+import { DatePicker } from 'react-nice-dates';
+import {enUS} from 'date-fns/locale';
+import 'react-nice-dates/build/style.css'
 // import { use } from "../../../../backend/routes/api/venues";
 
 import "./SplashPage.css";
@@ -11,6 +14,8 @@ function SplashPage() {
   const history = useHistory();
 
   const [searchInput, setSearchInput] = useState("");
+  const [date, setDate] = useState(new Date());
+
 
   const searchStarted = async () => {
     await dispatch(venueActions.searchVenues(searchInput));
@@ -19,24 +24,43 @@ function SplashPage() {
 
   return (
       <div className="splash">
-        <h1 className="header">Find yourselves outdoors</h1>
-        <h3 className="header-2">Come see what's waiting for you . . .</h3>
-        <div className="form-box">
-          <input
-            type="text"
-            className="search-field__location"
-            placeholder="Location..."
-            value={searchInput}
-            onChange={(event) => setSearchInput(event.target.value)}
-          />
-          <input type="date" className="search-field__dates"></input>
-          <button
-            className="search-button"
-            onClick={(event) => searchStarted()}
-            type="button"
-          >
-            <i className="fas fa-search"></i>
-          </button>
+        <div className="splash-grid">
+          <div className="splash-headers">
+            <h1 className="header-1">Find yourselves outdoors</h1>
+            <h3 className="header-2">Come see what's waiting for you . . .</h3>
+          </div>
+          <div className="splash-search">
+            <div className="search-box-grid">
+              <p className="location-text">Where to?</p>
+              <div className="location-box">
+                <input
+                  type="text"
+                  className="location-input"
+                  placeholder="Location..."
+                  value={searchInput}
+                  onChange={(event) => setSearchInput(event.target.value)}
+                />
+              </div>
+              <p className="date-text">Dates</p>
+              <div className="date-box">
+                <DatePicker date={date} onDateChange={setDate} locale={enUS}>
+                  {({ inputProps, focused }) => (
+                  <input
+                    className={'date-input' + (focused ? ' -focused' : '')}
+                    {...inputProps}
+                  />
+                  )}
+                </DatePicker>
+              </div>
+              <div className="search-button-box">
+                <button
+                  className="search-bar-button"
+                  onClick={(event) => searchStarted()}
+                  type="button"
+                ><i id="search-bar-icon" className="fas fa-search"></i></button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
   );
