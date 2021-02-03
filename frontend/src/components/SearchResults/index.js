3c69c6ce7dedbeb300d50944bdfaedab81ed101d
Jesse <jbenlind@gmail.com>
2021-02-03 11:02:03 -0600

    changes to search results

--- frontend/src/components/SearchResults/index.js
+++ frontend/src/components/SearchResults/index.js
@@ -7,20 +7,24 @@ const SearchResults = () => {
 const venues = useSelector((state) => state.venueInfo.venues)
     return(
         <div className="search-body">
-            <div className="search-header-container">
-                <p className="search-header">Locations found . . .</p>
-            </div>
-            <div className="search-grid-body">
-                {venues &&
-                venues.map((venue) => (
-                    <Link className ='card-link' key={venue.id} to={`/explore/${venue.id}`}>
-                        <div className="venue-card" value={venue.id} key={venue.id}
-                            style={ {backgroundImage: `url(${venue.imgUrl})`}}>
-                            <div className='card-title'>{venue.name}</div>
-                            <div className='card-location'>{`${venue.city}, ${venue.state}`}</div>
-                        </div>
-                    </Link>
-                ))}
+            <div className="search-body-grid">
+                <div className="card-holder-container">
+                    <div className="card-holder-grid">
+                        {venues &&
+                        venues.map((venue) => (
+                            <Link className ='card-link' key={venue.id} to={`/explore/${venue.id}`}>
+                                <div className="venue-card" value={venue.id} key={venue.id}
+                                    style={ {backgroundImage: `url(${venue.imgUrl})`}}>
+                                    <div className='card-title'>{venue.name}</div>
+                                    <div className='card-location'>{`${venue.city}, ${venue.state}`}</div>
+                                </div>
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+                <div className="future-map">
+
+                </div>
             </div>
         </div>
     )
