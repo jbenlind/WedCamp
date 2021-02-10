19b253cc4887281151ac18a3e734a792807279c4
Jesse <jbenlind@gmail.com>
2021-02-10 00:22:53 -0600

    adding review

--- backend/routes/api/venues.js
+++ backend/routes/api/venues.js
@@ -45,6 +45,28 @@ router.get(
     const venues = await Venue.findByPk(venueId)
     res.json(venues);
   })
+
 );
+
+router.post("/reviews", asyncHandler(async (req, res) => {
+  const {userId, venueId, rating, title, reviewBody, verifiedBooking} = req.body;
+  await Review.create({
+    userId,
+    venueId,
+    rating,
+    title,
+    body: reviewBody,
+    verifiedBooking
+  })
+  const venues = await Venue.findAll({
+    include: [
+      {model: Review,
+      include: {model:User}},
+      {model: Amenity, through: {attributes: []}}
+    ]}
+
+    );
+  res.json(venues);
+}) )
 
 module.exports = router;
