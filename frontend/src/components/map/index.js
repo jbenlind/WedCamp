2f714a42d1fbc00955a04d7f2e365cd8200cd4ca
Jesse <jbenlind@gmail.com>
2021-02-10 23:24:05 -0600

    google maps single

--- frontend/src/components/map/index.js
+++ frontend/src/components/map/index.js
@@ -1,30 +1,28 @@
 import React from "react";
-import GoogleMapReact from 'google-map-react';
+import { GoogleMap, Marker} from '@react-google-maps/api';
 
+const containerStyle = {
+  width: '448px',
+  height: '402px'
+};
 
-const Pin = () => {
-    return <i id="pin-icon" className="fad fa-map-pin" style={{fontSize: '35px', color: 'rgba(30, 130, 76, 1)'}}></i>;
-}
 const SingleVenueMap = ({venue}) => {
 
-    const defaultProps = {
-        center: {
-          lat: Number(venue.latitude),
-          lng: Number(venue.longitude)
-        },
-        zoom: 9
-    }
-            return (
-              <div className="map" style={{ height: '420px', width: '450px', borderRadius: "25px" }}>
-                <GoogleMapReact
-                  bootstrapURLKeys={{ key: process.env.REACT_APP_API_KEY}}
-                  center={defaultProps.center}
-                  defaultZoom={9}
-                >
-                <Pin lat={defaultProps.center.lat} lng={defaultProps.center.lng}/>
-                </GoogleMapReact>
-              </div>
-            );
+const center = {
+  lat: Number(venue.latitude),
+  lng: Number(venue.longitude)
+}
+
+    return (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={9}
+        >
+        <Marker position={{lat: Number(venue.latitude), lng: Number(venue.longitude)}}/>
+        </GoogleMap>
+
+    );
 
 }
 
